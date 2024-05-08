1b8bbd95b3c58ca832b3f1dc1817b77775668d0e
Cornexology <adedayocornex@gmail.com>
2024-05-08 06:57:14 +0100

    Added text to this commit

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
+    <div className="text-center mt-20 m-10 bg-white-600 w-screen h-screen">
     <h1 className="text-4xl font-bold text-indigo-600">Hello Cornex</h1>
+    <p className="text-blue-500 text-lg">This was meant to be part of the push request </p>
+    
+    </div>
   );
 }
